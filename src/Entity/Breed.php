43608df12819ff30b293ca1d00877081fd94f081
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-13 17:48:25 +0200

    Added constraints to property.

--- src/Entity/Breed.php
+++ src/Entity/Breed.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BreedRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BreedRepository::class)
@@ -19,11 +20,23 @@ class Breed
 
     /**
      * @ORM\Column(type="string", length=20)
+     * @Assert\Length(
+     *     min = 4,
+     *     max = 20
+     *     minMessage = "breed.name.min",
+     *     maxMessage = "breed.name.max"
+     * )
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=20)
+     * @Assert\Length(
+     *     min = 4,
+     *     max = 20
+     *     minMessage = "breed.type.min",
+     *     maxMessage = "breed.type.max"
+     * )
      */
     private $type;
 
