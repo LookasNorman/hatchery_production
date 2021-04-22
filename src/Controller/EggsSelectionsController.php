7d21ada80cd0bfafed2ee2f284b252a7918eda2a
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-22 21:20:50 +0200

    Customized eggs inputs selection added to DB.

--- src/Controller/EggsSelectionsController.php
+++ src/Controller/EggsSelectionsController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\EggsInputs;
 use App\Entity\EggsSelections;
+use App\Entity\EggSupplier;
 use App\Form\EggsSelectionsType;
+use App\Repository\EggsInputsDetailsRepository;
 use App\Repository\EggsSelectionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,22 +34,51 @@ class EggsSelectionsController extends AbstractController
      * @Route("/new", name="eggs_selections_new", methods={"GET","POST"})
      * @IsGranted("ROLE_ADMIN")
      */
-    public function new(Request $request): Response
+    public function new(Request $request, EggsInputsDetailsRepository $detailsRepository): Response
     {
-        $eggsSelection = new EggsSelections();
-        $form = $this->createForm(EggsSelectionsType::class, $eggsSelection);
+        $form = $this->createForm(EggsSelectionsType::class);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if (
+            $form->isSubmitted() &&
+            $form->isValid() &&
+            $form['breeder']->getData() instanceof EggSupplier &&
+            $form['eggsInputs']->getData() instanceof EggsInputs
+        ) {
+            $totalEggs = 0;
+            $inputs = $form['eggsInputs']->getData();
+            $breeder = $form['breeder']->getData();
+            $chickNumber = $form['chickNumber']->getData();
+            $cullChicken = $form['cullChicken']->getData();
+            $selectionDate = $form['selectionDate']->getData();
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($eggsSelection);
+            $inputsDetails = $detailsRepository->inputBreederDetails($inputs, $breeder);
+
+            /** @var  $inputDetail
+             * Get sum eggs for breeder in eggs input
+             */
+            foreach ($inputsDetails as $inputDetail) {
+                $totalEggs = $totalEggs + $inputDetail[1];
+            }
+
+            /** @var  $inputDetail
+             * Added eggs transfer to eggs input details
+             */
+            foreach ($inputsDetails as $inputDetail) {
+                $eggsSelection = new EggsSelections();
+                $eggsSelection->setEggsInputsDetail($inputDetail[0]);
+                $eggsSelection->setChickNumber($inputDetail[1] / $totalEggs * $chickNumber);
+                $eggsSelection->setCullChicken($inputDetail[1] / $totalEggs * $cullChicken);
+                $eggsSelection->setSelectionDate($selectionDate);
+                $entityManager->persist($eggsSelection);
+            }
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('eggs_selections_index');
+            return $this->redirectToRoute('eggs_inputs_show', ['id' => $inputs->getId()]);
         }
 
         return $this->render('eggs_selections/new.html.twig', [
-            'eggs_selection' => $eggsSelection,
             'form' => $form->createView(),
         ]);
     }
