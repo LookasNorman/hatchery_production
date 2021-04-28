94885a0eaec5d03562283d5daf52b607c6ca85e2
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-28 21:39:41 +0200

    Added incubators list page. Added link to menu.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\HatchersRepository;
+use App\Repository\SettersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class DefaultController extends AbstractController
 {
@@ -14,6 +17,23 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('main_page/index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @param \App\Repository\SettersRepository $settersRepository
+     * @param \App\Repository\HatchersRepository $hatchersRepository
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/incubators", name="incubators_index")
+     * @IsGranted("ROLE_USER")
+     */
+    public function incubators(SettersRepository $settersRepository, HatchersRepository $hatchersRepository)
+    {
+        $setters = $settersRepository->findAll();
+        $hatchers = $hatchersRepository->findAll();
+        return $this->render('incubators/index.html.twig', [
+            'setters' => $setters,
+            'hatchers' => $hatchers,
         ]);
     }
 }
