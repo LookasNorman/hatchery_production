58ec1d0b59f5ea68339defe8401d3ba5b2f3a52d
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-04-28 18:57:23 +0200

    Added entity for setters incubators.

---
+++ src/Entity/Setters.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SettersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SettersRepository::class)
+ */
+class Setters
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=4)
+     */
+    private $shortname;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getShortname(): ?string
+    {
+        return $this->shortname;
+    }
+
+    public function setShortname(string $shortname): self
+    {
+        $this->shortname = $shortname;
+
+        return $this;
+    }
+}
