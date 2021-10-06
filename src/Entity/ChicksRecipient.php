97289d83941701f9796ea000ebfd1802baf8a90b
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-10-06 11:07:08 +0200

    Added address to customer and chicks recipient.

--- src/Entity/ChicksRecipient.php
+++ src/Entity/ChicksRecipient.php
@@ -63,6 +63,26 @@ class ChicksRecipient
      */
     private $customer;
 
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $postcode;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $streetNumber;
+
     public function __construct()
     {
         $this->eggsInputsDetails = new ArrayCollection();
@@ -147,6 +167,54 @@ class ChicksRecipient
     public function setCustomer(?Customer $customer): self
     {
         $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getPostcode(): ?string
+    {
+        return $this->postcode;
+    }
+
+    public function setPostcode(?string $postcode): self
+    {
+        $this->postcode = $postcode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(?string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getStreetNumber(): ?string
+    {
+        return $this->streetNumber;
+    }
+
+    public function setStreetNumber(?string $streetNumber): self
+    {
+        $this->streetNumber = $streetNumber;
 
         return $this;
     }
--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -39,6 +39,26 @@ class Customer
      */
     private $chicksRecipients;
 
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $postCode;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $streetNumber;
+
     public function __construct()
     {
         $this->chicksRecipients = new ArrayCollection();
@@ -111,6 +131,54 @@ class Customer
                 $chicksRecipient->setCustomer(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getPostCode(): ?string
+    {
+        return $this->postCode;
+    }
+
+    public function setPostCode(?string $postCode): self
+    {
+        $this->postCode = $postCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(?string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getStreetNumber(): ?string
+    {
+        return $this->streetNumber;
+    }
+
+    public function setStreetNumber(?string $streetNumber): self
+    {
+        $this->streetNumber = $streetNumber;
 
         return $this;
     }
