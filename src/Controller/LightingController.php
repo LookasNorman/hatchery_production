1696139d944ae7ad5ec1a12d9096c11a32bd36e6
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-10-08 21:36:59 +0200

    Divide new lighting to three separate function. Change format view.

--- src/Controller/LightingController.php
+++ src/Controller/LightingController.php
@@ -31,6 +31,66 @@ class LightingController extends AbstractController
         ]);
     }
 
+    public function totalEggs($inputsFarmDelivery)
+    {
+        $totalEggs = 0;
+        foreach ($inputsFarmDelivery as $inputFarmDelivery) {
+            $totalEggs = $totalEggs + $inputFarmDelivery->getEggsNumber();
+        }
+        return $totalEggs;
+    }
+
+    public function createLighting($form, $inputsFarmDelivery )
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $wasteEggs = $form['wasteEggs']->getData();
+        $lightingDate = $form['lightingDate']->getData();
+        $eggsNumber = $form['eggsNumber']->getData();
+        $totalEggs = $this->totalEggs($inputsFarmDelivery);
+
+        $totalWaste = 0;
+        $totalEggsNumber = 0;
+        $length = count($inputsFarmDelivery) - 1;
+
+        foreach ($inputsFarmDelivery as $key => $inputFarmDelivery) {
+            $eggsInputLighting = new Lighting();
+            $eggsInputLighting->setLightingDate($lightingDate);
+            if ($totalEggs === $eggsNumber) {
+                $setEggs = $inputFarmDelivery->getEggsNumber();
+                $eggsInputLighting->setLightingEggs($setEggs);
+            } else {
+                if ($key < $length) {
+                    $setEggs = round($eggsNumber / $totalEggs * $inputFarmDelivery->getEggsNumber());
+                    $eggsInputLighting->setLightingEggs($setEggs);
+                    $totalEggsNumber = $totalEggsNumber + $setEggs;
+                } else {
+                    $setEggs = $eggsNumber - $totalEggsNumber;
+                    $eggsInputLighting->setLightingEggs($setEggs);
+                }
+            }
+            $eggsInputLighting->addInputsFarmDelivery($inputFarmDelivery);
+
+            if ($key < $length) {
+                $setWaste = round($inputFarmDelivery->getEggsNumber() / $totalEggs * $wasteEggs, 0);
+                $eggsInputLighting->setWasteEggs($setWaste);
+                $totalWaste = $totalWaste + $setWaste;
+            } else {
+                $setWaste = $wasteEggs - $totalWaste;
+                $eggsInputLighting->setWasteEggs($setWaste);
+            }
+
+            $fertility = round($setWaste / $setEggs + $inputFarmDelivery->getDelivery()->getHerd()->getLighting() / 100, 3);
+
+            $wasteLighting = $inputFarmDelivery->getEggsNumber() * $fertility;
+            $eggsInputLighting->setWasteLighting($wasteLighting);
+
+            $entityManager->persist($eggsInputLighting);
+        }
+        $entityManager->flush();
+
+    }
+
     /**
      * @Route("/new/{inputs}/{herd}", name="eggs_inputs_lighting_new", methods={"GET","POST"})
      * @IsGranted("ROLE_ADMIN")
@@ -50,63 +110,17 @@ class LightingController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $totalEggs = 0;
-            $wasteEggs = $form['wasteEggs']->getData();
-            $lightingDate = $form['lightingDate']->getData();
-            $eggsNumber = $form['eggsNumber']->getData();
-            $entityManager = $this->getDoctrine()->getManager();
             $inputsFarmDelivery = $inputsFarmDeliveryRepository->findByExampleField($inputs, $herd);
 
-            foreach ($inputsFarmDelivery as $inputFarmDelivery) {
-                $totalEggs = $totalEggs + $inputFarmDelivery->getEggsNumber();
-            }
-
-            $totalWaste = 0;
-            $totalEggsNumber = 0;
-            $length = count($inputsFarmDelivery) - 1;
-
-            foreach ($inputsFarmDelivery as $key => $inputFarmDelivery) {
-                $eggsInputLighting = new Lighting();
-                $eggsInputLighting->setLightingDate($lightingDate);
-                if ($totalEggs === $eggsNumber) {
-                    $setEggs = $inputFarmDelivery->getEggsNumber();
-                    $eggsInputLighting->setLightingEggs($setEggs);
-                } else {
-                    if ($key < $length) {
-                        $setEggs = round($eggsNumber / $totalEggs * $inputFarmDelivery->getEggsNumber());
-                        $eggsInputLighting->setLightingEggs($setEggs);
-                        $totalEggsNumber = $totalEggsNumber + $setEggs;
-                    } else {
-                        $setEggs = $eggsNumber - $totalEggsNumber;
-                        $eggsInputLighting->setLightingEggs($setEggs);
-                    }
-                }
-                $eggsInputLighting->addInputsFarmDelivery($inputFarmDelivery);
-
-                if ($key < $length) {
-                    $setWaste = round($inputFarmDelivery->getEggsNumber() / $totalEggs * $wasteEggs, 0);
-                    $eggsInputLighting->setWasteEggs($setWaste);
-                    $totalWaste = $totalWaste + $setWaste;
-                } else {
-                    $setWaste = $wasteEggs - $totalWaste;
-                    $eggsInputLighting->setWasteEggs($setWaste);
-                }
-
-                $fertility = round($setWaste / $setEggs + $inputFarmDelivery->getDelivery()->getHerd()->getLighting() / 100, 3);
-
-                $wasteLighting = $inputFarmDelivery->getEggsNumber() * $fertility;
-                $eggsInputLighting->setWasteLighting($wasteLighting);
-
-                $entityManager->persist($eggsInputLighting);
-            }
-
-            $entityManager->flush();
+            $this->createLighting($form, $inputsFarmDelivery);
 
             return $this->redirectToRoute('eggs_inputs_show', ['id' => $inputs->getId()]);
         }
 
         return $this->render('eggs_inputs_lighting/new.html.twig', [
             'form' => $form->createView(),
+            'input' => $inputs,
+            'herd' => $herd
         ]);
     }
 
