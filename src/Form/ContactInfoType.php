280e65839b34597b1ee2af2de8efad21bcfe02c9
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-11-03 18:51:55 +0100

    Formated contact info sites.

--- src/Form/ContactInfoType.php
+++ src/Form/ContactInfoType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\ContactInfo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,30 @@ class ContactInfoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('department')
-            ->add('person')
-            ->add('phoneNumber')
-            ->add('email')
+            ->add('department', TextType::class, [
+                'label' => 'contact_info.form.label.department',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('person', TextType::class, [
+                'label' => 'contact_info.form.label.person',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'contact_info.form.label.phone_number',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'contact_info.form.label.email',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
         ;
     }
 
