689e4346a4acf6eb7b254894d623d56fc6785216
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-11-04 12:38:23 +0100

    Added CRUD for User.

---
+++ src/Form/User1Type.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class User1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'registration.form.label.email',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'required' => false,
+                'label' => 'registration.form.label.firstname',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'required' => false,
+                'label' => 'registration.form.label.lastname',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'required' => false,
+                'label' => 'registration.form.label.phone_number',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
