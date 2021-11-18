4d081bbc7703295e724b0890dd35da428f195e98
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-11-18 20:33:45 +0100

    Added view and send email with customer chick plans.

--- src/Controller/PlanDeliveryChickController.php
+++ src/Controller/PlanDeliveryChickController.php
@@ -3,15 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\ChicksRecipient;
+use App\Entity\ContactInfo;
+use App\Entity\Customer;
 use App\Entity\PlanDeliveryChick;
 use App\Form\BetweenDateType;
 use App\Form\PlanDeliveryChickType;
 use App\Repository\PlanDeliveryChickRepository;
 use App\Repository\PlanIndicatorsRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -168,5 +172,67 @@ class PlanDeliveryChickController extends AbstractController
         }
 
         return $this->redirectToRoute('plan_delivery_chick_index');
+    }
+
+    public function footerCustomerPlans()
+    {
+        $contactInfoRepository = $this->getDoctrine()->getRepository(ContactInfo::class);
+        $hatchery = $contactInfoRepository->findOneBy(['department' => 'Wylęgarnia']);
+        $sales = $contactInfoRepository->findOneBy(['department' => 'Handel']);
+        $accounting = $contactInfoRepository->findOneBy(['department' => 'Księgowość']);
+        return ['hatchery' => $hatchery, 'sales' => $sales, 'accounting' => $accounting];
+    }
+
+    /**
+     * @Route("/customer_plans/send/{id}", name="customer_plans_send", methods={"GET"})
+     */
+    public function sendEmail(Customer $customer, MailerInterface $mailer)
+    {
+        $emailAddress = $customer->getEmail();
+        $plans = $this->customerPlan($customer);
+        $footer = $this->footerCustomerPlans();
+        $email = (new TemplatedEmail())
+            ->to('lkonieczny@zwdmalec.pl')
+            ->subject('Plan zamówień piskląt ' . $customer->getName())
+            ->htmlTemplate('emails/customerPlans.html.twig')
+            ->context([
+                'plans' => $plans,
+                'footer' => $footer,
+            ]);
+//        if ($emailAddress) {
+//            $email->addTo($emailAddress);
+//        }
+        $mailer->send($email);
+        return $this->redirectToRoute('customer_index');
+    }
+
+    public function customerPlan(Customer $customer)
+    {
+        $farmRepository = $this->getDoctrine()->getRepository(ChicksRecipient::class);
+        $planChickRepository = $this->getDoctrine()->getRepository(PlanDeliveryChick::class);
+        $farms = $farmRepository->findBy(['customer' => $customer], ['name' => 'ASC']);
+        $plans = [];
+        foreach ($farms as $farm){
+            $farmPlans = $planChickRepository->findBy(['chickFarm' => $farm], ['inputDate' => 'ASC']);
+            array_push($plans, ['farm' => $farm, 'farmPlans' => $farmPlans]);
+        }
+        return $plans;
+    }
+
+    /**
+     * @param \App\Entity\Customer $customer
+     * @Route("/customer_plans/{id}", name="customer_plans", methods={"GET"})
+     */
+    public function sendPlan(Customer $customer)
+    {
+        $plans = $this->customerPlan($customer);
+        $footer = $this->footerCustomerPlans();
+
+        return $this->render('emails/customerPlans.html.twig', [
+            'customer' => $customer,
+            'plans' => $plans,
+            'footer' => $footer,
+            'send' => true
+        ]);
     }
 }
