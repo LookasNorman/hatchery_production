d6946b9ad348967d0dd58bafe304d924e5684140
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-11-19 09:48:56 +0100

    Added send info about input farm by email.

--- src/Controller/InputsFarmController.php
+++ src/Controller/InputsFarmController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactInfo;
 use App\Entity\Inputs;
 use App\Entity\InputsFarm;
 use App\Form\InputsFarmType;
-use App\Repository\InputsFarmRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -23,7 +25,7 @@ class InputsFarmController extends AbstractController
      * @Route("/new/{id}", name="inputs_farm_new", methods={"GET","POST"})
      * @IsGranted("ROLE_MANAGER")
      */
-    public function new(Request $request, Inputs $inputs = null): Response
+    public function new(Request $request, Inputs $inputs = null, MailerInterface $mailer): Response
     {
         $inputsFarm = new InputsFarm();
         $inputsFarm->setEggInput($inputs);
@@ -33,6 +35,10 @@ class InputsFarmController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($inputsFarm);
+            $email = $this->sendEmail($inputsFarm);
+            if ($email) {
+                $mailer->send($email);
+            }
             $entityManager->flush();
 
             return $this->redirectToRoute('eggs_inputs_show', ['id' => $inputs->getId()]);
@@ -42,6 +48,33 @@ class InputsFarmController extends AbstractController
             'inputs_farm' => $inputsFarm,
             'form' => $form->createView(),
         ]);
+    }
+
+    public function sendEmail($inputsFarm)
+    {
+        $contactInfoRepository = $this->getDoctrine()->getRepository(ContactInfo::class);
+        $hatchery = $contactInfoRepository->findOneBy(['department' => 'Wylęgarnia']);
+        $sales = $contactInfoRepository->findOneBy(['department' => 'Handel']);
+        $accounting = $contactInfoRepository->findOneBy(['department' => 'Księgowość']);
+        $emailAddress = null;
+//        $emailAddress = $inputsFarm->getChicksFarm()->getEmail();
+
+        $email = (new TemplatedEmail())
+            ->to('rgolec@zwdmalec.pl')
+            ->addTo('kkrakowiak@zwdmalec.pl')
+            ->addBcc('lkonieczny@zwdmalec.pl')
+            ->subject('Wprowadzono nakład ' . $inputsFarm->geteggInput()->getInputDate()->format('Y-m-d'))
+            ->htmlTemplate('emails/inputFarm.html.twig')
+            ->context([
+                'inputFarm' => $inputsFarm,
+                'hatchery' => $hatchery,
+                'sales' => $sales,
+                'accounting' => $accounting
+            ]);
+        if ($emailAddress) {
+            $email->addTo($emailAddress);
+        }
+        return $email;
     }
 
     /**
